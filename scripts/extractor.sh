ad34b155f3d343bad928cac57e3e252518efbd2e
leifos <leifos@acm.org>
2017-05-10 17:40:28 +0100

    Added in the test files

--- scripts/extractor.sh
+++ scripts/extractor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 re='[0-9]+$'
 
-for filename in topics_train/*; do
+for filename in testing/topics/*; do
    echo $filename
    if ! [[ $filename =~ $re ]] ; then
    	echo $filename "  ignored" >&2; 
    else
-	python extract_both.py $filename "TOP"
+	python scripts/extract_parts_from_topic.py $filename "TOP"
    fi
 done
