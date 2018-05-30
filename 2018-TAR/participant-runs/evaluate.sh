fbf261568f3e0f804da25d03cfcce775fcac3791
Leifos <leifos@acm.org>
2018-05-30 16:47:47 +0100

    Added a recall at threshold measure to task 1 ruler

--- 2018-TAR/participant-runs/evaluate.sh
+++ 2018-TAR/participant-runs/evaluate.sh
@@ -15,7 +15,7 @@ fi
 
 
 touch task1.out.all
-echo "Run MAP recall@50 recall@100 recall@200 recall@300 recall@400 recall@500 recall@1000 recall@2000" > task1.out.all
+echo "Run MAP recall@50 recall@100 recall@200 recall@300 recall@400 recall@500 recall@1000 recall@2000 recall@5000 recall@threshold threshold" > task1.out.all
 
 for filename in `find -f * | grep "task1$"`; do
    echo $filename
@@ -24,7 +24,7 @@ for filename in `find -f * | grep "task1$"`; do
    while read  qid metric score ; do
         hashmap["$metric"]="$score"
    done < ${filename}".out"
-   echo "${filename} ${hashmap["ap"]}  ${hashmap["recall@50"]} ${hashmap["recall@100"]} ${hashmap["recall@200"]} ${hashmap["recall@300"]} ${hashmap["recall@400"]} ${hashmap["recall@500"]} ${hashmap["recall@1000"]} ${hashmap["recall@2000"]}" >> task1.out.all
+   echo "${filename} ${hashmap["ap"]}  ${hashmap["recall@50"]} ${hashmap["recall@100"]} ${hashmap["recall@200"]} ${hashmap["recall@300"]} ${hashmap["recall@400"]} ${hashmap["recall@500"]} ${hashmap["recall@1000"]} ${hashmap["recall@2000"]} ${hashmap["recall@5000"]} ${hashmap["recall_at_threshold"]} ${hashmap["recall_threshold"]}" >> task1.out.all
 done
 
 
